4d68faf6198395dc888f2b4bb20fd082a5c7cfc8
pkesseli <pkesseli@dkr13.cs.ox.ac.uk>
2016-06-27 16:31:21 +0100

    Updated reflector usage of "declared fields".

--- src/test_gen/Reflector.java
+++ src/test_gen/Reflector.java
@@ -1,10 +1,12 @@
 package com.diffblue.java_testcase;
 
-import org.objenesis.Objenesis;
-import org.objenesis.ObjenesisStd;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.objenesis.ObjenesisStd;
 
 /**
  * Describe class <code>Reflector</code> here.
@@ -12,8 +14,26 @@ import java.lang.reflect.Field;
  * @author Matthias Guedemann
  * @version 1.0
  */
-public class Reflector
+public final class Reflector
 {
+  private Reflector() {}
+
+  private static <T> void setInstanceField(Class<T> c, Object o, String fieldName, Object newVal) throws NoSuchFieldException, IllegalArgumentException
+  {
+    if (c == null) throw new NoSuchFieldException();
+    Optional<Field> field = Arrays.stream(c.getDeclaredFields()).filter(f -> f.getName().equals(fieldName)).findAny();
+    if (!field.isPresent()) setInstanceField(c.getSuperclass(), o, fieldName, newVal);
+    else
+    {
+      Field property = field.get();
+      property.setAccessible(true);
+      try {
+        property.set(o, newVal);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e); // Should never happen.
+      }
+    }
+  }
 
   /**
    * Changes a given field of an object instance via reflection, bypassing the
@@ -22,41 +42,13 @@ public class Reflector
    * @param o an <code>Object</code> instance to change
    * @param fieldName a <code>String</code> the name of the field to change
    * @param newVal an <code>Object</code> the new value for the field
+   * 
+   * @throws NoSuchFieldException if a field with the specified name is not found.
+   * @throws IllegalArgumentException if the specified object is not an instance of the class or interface declaring the underlying field (or a subclass or implementor thereof), or if an unwrapping conversion fails.
    */
-  public void setInstanceField(Object o, String fieldName, Object newVal)
+  public static void setInstanceField(Object o, String fieldName, Object newVal) throws NoSuchFieldException, IllegalArgumentException
   {
-    Field f;
-
-    try
-    {
-      f = o.getClass().getField(fieldName);
-    }
-    catch (NoSuchFieldException nsf)
-    {
-      // field is not public
-      try
-      {
-        f = o.getClass().getDeclaredField(fieldName);
-      }
-      catch (NoSuchFieldException nsf2)
-      {
-        // Field does not exist -> do nothing
-	  System.out.println("Field not found");  
-        return;
-      }
-    }
-
-    try
-    {
-    	f.setAccessible(true);
-    	f.set(o, newVal);
-    }
-    catch (IllegalAccessException ie)
-    {
-    	System.out.println("ERROR: illegal access, field " + fieldName +
-    			" cannot be set");
-    	System.out.println(ie);
-    }
+    setInstanceField(o.getClass(), o, fieldName, newVal);
   }
 
   /**
@@ -64,34 +56,17 @@ public class Reflector
    *
    * @param className a <code>String</code> giving the name of the class
    * @return an <code>Object</code> which is an instance of the specified class
-   * @exception ClassNotFoundException if the class cannot be found in the
+   * @throws ClassNotFoundException if the class cannot be found in the
    * classpath
    */
-  public Object forceInstance(String className) throws ClassNotFoundException
+  public static Object forceInstance(String className) throws ClassNotFoundException
   {
-    Class c = Class.forName(className);
-    return forceInstance(c);
+    return forceInstance(Class.forName(className));
   }
 
-  private boolean hasDefaultConstructor(Class c)
+  private static Optional<Constructor<?>> getDefaultConstructor(Class<?> c)
   {
-    for(Constructor ctor : c.getConstructors())
-      if (ctor.getParameterTypes().length == 0)
-        return true;
-
-    return false;
-  }
-
-  private boolean hasDefaultPrivateConstructor(Class c)
-  {
-    for(Constructor ctor : c.getDeclaredConstructors())
-      if (ctor.getParameterTypes().length == 0)
-      {
-        ctor.setAccessible(true);
-        return true;
-      }
-
-    return false;
+    return Arrays.stream(c.getDeclaredConstructors()).filter(ctor -> ctor.getParameterCount() == 0).findAny();
   }
 
   /**
@@ -102,22 +77,17 @@ public class Reflector
    * @param c a <code>Class</code> the class to instantiate
    * @return an <code>Object</code> which is an instance of the specified class
    */
-  public Object forceInstance(Class c)
+  @SuppressWarnings("unchecked")
+  public static <T> T forceInstance(Class<T> c)
   {
-	
-    try
+    Optional<Constructor<?>> ctor = getDefaultConstructor(c);
+    if (ctor.isPresent())
     {
-      if (hasDefaultConstructor(c))
-        return c.newInstance();
-      else if (hasDefaultPrivateConstructor(c))
-        return c.newInstance();
+      Constructor<?> defaultCtor = ctor.get();
+      defaultCtor.setAccessible(true);
+      try { return (T) defaultCtor.newInstance(); }
+      catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {}
     }
-    catch (InstantiationException ie) {}
-    catch (IllegalAccessException iae) {}
-    
-
-    Objenesis objenesis = new ObjenesisStd();
-    Object o = objenesis.newInstance(c);
-    return o;
+    return new ObjenesisStd().newInstance(c);
   }
 }
