f5bffdadb31b585df080717a5784caef505d79e4
Chris Smowton <chris@smowton.net>
2016-07-19 16:49:54 +0100

    Add java support class

---
+++ src/test_gen/IterAnswer.java
@@ -0,0 +1,19 @@
+import org.mockito.stubbing.Answer;
+import org.mockito.invocation.InvocationOnMock;
+import java.util.ArrayList;
+
+public class IterAnswer<T> implements Answer<T> {
+
+    private int idx = 0;
+    private ArrayList<T> answers;
+
+    public IterAnswer(ArrayList<T> _answers) {
+	answers = _answers;
+    }
+    
+    public T answer(InvocationOnMock invocation) {
+	return answers.get(idx++);
+    }    
+
+}
+
