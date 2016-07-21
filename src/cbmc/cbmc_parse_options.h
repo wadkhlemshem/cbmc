a1828690b2a4ec0eafa26846a5f54eed67037ef8
Cristina <cristina.david@gmail.com>
2016-07-21 17:11:21 +0100

    actually adding the new flag

--- src/cbmc/cbmc_parse_options.h
+++ src/cbmc/cbmc_parse_options.h
@@ -45,7 +45,7 @@ class optionst;
   "(stop-on-fail)(trace)" \
   "(error-label):(verbosity):(no-library)" \
   "(version)" \
-  "(cover):" \
+  "(cover):(cover-function-only)" \
   "(mm):" \
   "(i386-linux)(i386-macos)(i386-win32)(win32)(winx64)(gcc)" \
   "(ppc-macos)(unsigned-char)" \
