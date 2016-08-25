855e5220ab28ee8c8090dc7831fc8d4631c68bc0
Matthias Güdemann <matthias.gudemann@gmail.com>
2016-08-25 15:02:22 +0200

    load java.lang.String in every case

--- src/java_bytecode/java_class_loader.cpp
+++ src/java_bytecode/java_class_loader.cpp
@@ -38,6 +38,8 @@ java_bytecode_parse_treet &java_class_loadert::operator()(
   // Always require java.lang.Object, as it is the base of
   // internal classes such as array types.
   queue.push("java.lang.Object");
+  // java.lang.String
+  queue.push("java.lang.String");
   queue.push(class_name);
 
   while(!queue.empty())
