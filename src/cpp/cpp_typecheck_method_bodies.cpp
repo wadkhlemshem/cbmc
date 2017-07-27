0e368b08f4d2897734ddac434f200432681d9f77
Peter Schrammel <peter.schrammel@diffblue.com>
2017-07-27 16:26:32 +0200

    Fixup! method bodies

--- src/cpp/cpp_typecheck_method_bodies.cpp
+++ src/cpp/cpp_typecheck_method_bodies.cpp
@@ -6,30 +6,57 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 
 \*******************************************************************/
 
+// #define DEBUG
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
 /// \file
 /// C++ Language Type Checking
 
 #include "cpp_typecheck.h"
 
-void cpp_typecheckt::typecheck_method_bodies(
-  method_bodiest &bodies)
+void cpp_typecheckt::typecheck_method_bodies()
 {
   instantiation_stackt old_instantiation_stack;
   old_instantiation_stack.swap(instantiation_stack);
 
-  for(auto &b : bodies)
+  while(!method_bodies.empty())
   {
-    symbolt &method_symbol=*b.method_symbol;
-    template_map.swap(b.template_map);
-    instantiation_stack.swap(b.instantiation_stack);
+    //Dangerous not to take a copy here. We'll have to make sure that
+    //  convert is never called with the same symbol twice.
+#if 1
+    method_bodyt &method_body = *method_bodies.begin();
+#else
+    method_bodyt &method_body = method_bodies.begin()->second;
+#endif
+    symbolt &method_symbol = *method_body.method_symbol;
+
+    template_map.swap(method_body.template_map);
+    instantiation_stack.swap(method_body.instantiation_stack);
+
+#if 0
+    std::cout << "MAP for " << method_symbol.name << ":" << std::endl;
+    template_map.print(std::cout);
+#endif
+    method_bodies.erase(method_bodies.begin());
 
     if(method_symbol.name==ID_main)
+    {
       add_argc_argv(method_symbol);
+      method_symbol.mode=ID_cpp; //main has mode C by default
+    }
 
     exprt &body=method_symbol.value;
     if(body.id()=="cpp_not_typechecked")
       continue;
 
+#ifdef DEBUG
+  std::cout << "convert_method_body: " << method_symbol.name << std::endl;
+  std::cout << "  is_not_nil: " << body.is_not_nil() << std::endl;
+  std::cout << "  !is_zero: " << (!body.is_zero()) << std::endl;
+#endif
     if(body.is_not_nil() &&
        !body.is_zero())
     {
@@ -38,4 +65,52 @@ void cpp_typecheckt::typecheck_method_bodies(
   }
 
   old_instantiation_stack.swap(instantiation_stack);
+}
+
+void cpp_typecheckt::add_method_body(symbolt *_method_symbol)
+{
+#ifdef DEBUG
+  std::cout << "add_method_body: " << _method_symbol->name << std::endl;
+#endif
+  //TODO: cleanup
+#if 1
+  // We have to prevent the same method to be added multiple times
+  //   otherwise we get duplicated symbol prefixes
+  if(methods_seen.find(_method_symbol->name) != methods_seen.end())
+  {
+#ifdef DEBUG
+    std::cout << "  already exists" << std::endl;
+#endif
+    return;
+  }
+  method_bodies.push_back(method_bodyt(
+			      _method_symbol, template_map, instantiation_stack));
+
+
+  // Converting a function body might add methods bodies for methods
+  // that we have already analyzed. Hence, we have to keep track.
+  methods_seen.insert(_method_symbol->name);
+#else
+  if(method_bodies.find(_method_symbol->name) != method_bodies.end())
+  {
+#ifdef DEBUG
+    std::cout << "  already exists" << std::endl;
+    if(method_bodies[_method_symbol->name].method_symbol->value !=
+       _method_symbol->value)
+    {
+      std::cout << "  OLD: "
+                << method_bodies[_method_symbol->name]
+                     .method_symbol->value.pretty() << std::endl;
+      std::cout << "  NEW: " << _method_symbol->value.pretty() << std::endl;
+    }
+#endif
+  }
+  else
+  {
+    method_bodyt &method_body = method_bodies[_method_symbol->name];
+    method_body.method_symbol = _method_symbol;
+    method_body.template_map = template_map;
+    method_body.instantiation_stack = instantiation_stack;
+  }
+#endif
 }
