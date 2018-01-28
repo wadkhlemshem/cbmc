c81f7067055b533ede26e289aa36f751353e288d
Peter Schrammel <peter.schrammel@diffblue.com>
2018-01-28 12:37:08 +0000

    Regression test for failing switch on Enum

---
+++ regression/cbmc-java/enum3/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public enum MyEnum {
+    A, B, C };
+  
+  public static MyEnum e = MyEnum.B;
+
+  public static void main(String[] args) {
+    int i = 0;
+    switch(e) {
+    case A: i = 2; break;
+    case B: i = 4; break;
+    case C: i = 8; break;
+    }
+    assert(i == 2);
+  }
+}
