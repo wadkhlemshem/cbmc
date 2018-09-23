07bdf84af7077aa5b9d4fa201a7381530051ae19
Peter Schrammel <peter.schrammel@diffblue.com>
2018-09-23 21:31:17 +0100

    Implement stop_on_fail_verifier

--- src/goto-checker/stop_on_fail_verifier.h
+++ src/goto-checker/stop_on_fail_verifier.h
@@ -26,16 +26,53 @@ public:
     goto_model(goto_model),
     goto_checker(options, ui_message_handler, goto_model)
   {
+    properties = std::move(initialize_properties(goto_model));
   }
 
   resultt operator()() override
   {
-    return resultt::UNKNOWN;
+    // have we got anything to check? otherwise we return PASS
+    if(!has_properties_to_check(properties))
+      return resultt::PASS;
+
+    (void)goto_checker(properties);
+    return determine_result(properties);
   }
+
+  void report() override;
 
 protected:
   abstract_goto_modelt &goto_model;
   goto_checkerT goto_checker;
 };
+
+template<class goto_checkerT>
+void stop_on_fail_verifiert::report()
+{
+  switch(determine_result(properties))
+  {
+    case resultt::PASS:
+      report_success(ui_message_handler);
+      goto_checker.output_proof();
+      break;
+
+    case resultt::FAIL:
+    {
+      goto_tracet error_trace = goto_checker.build_error_trace();
+      output_error_trace(error_trace, ns, trace_options(), ui_message_handler);
+      report_failure(ui_message_handler);
+      goto_checker.output_error_witness(error_trace);
+      break;
+    }
+
+    case resultt::UNKNOWN:
+      report_inconclusive(ui_message_handler);
+      break;
+
+    case resultt::ERROR:
+      report_error(ui_message_handler);
+      break;
+  }
+}
 
 #endif // CPROVER_GOTO_CHECKER_STOP_ON_FAIL_VERIFIER_H
