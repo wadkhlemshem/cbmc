11b823079a96f19dd83e31e59da07d58492dd708
Peter Schrammel <peter.schrammel@diffblue.com>
2018-09-23 21:48:36 +0100

    Fix stoponfail templ

--- src/goto-checker/stop_on_fail_verifier.h
+++ src/goto-checker/stop_on_fail_verifier.h
@@ -14,14 +14,16 @@ Author: Daniel Kroening, Peter Schrammel
 
 #include "goto_verifier.h"
 
+#include "bmc_util.h"
+
 template<class goto_checkerT>
 class stop_on_fail_verifiert : public goto_verifiert
 {
 public:
   stop_on_fail_verifiert(
-    const optionst &,
-    ui_message_handlert &,
-    abstract_goto_modelt &)
+    const optionst &options,
+    ui_message_handlert &ui_message_handler,
+    abstract_goto_modelt &goto_model)
   : goto_verifiert(options, ui_message_handler),
     goto_model(goto_model),
     goto_checker(options, ui_message_handler, goto_model)
@@ -39,40 +41,38 @@ public:
     return determine_result(properties);
   }
 
-  void report() override;
+  void report() override
+  {
+    switch(determine_result(properties))
+    {
+      case resultt::PASS:
+        report_success(ui_message_handler);
+        goto_checker.output_proof();
+        break;
+
+      case resultt::FAIL:
+      {
+        namespacet ns(goto_model.get_symbol_table());
+        goto_tracet error_trace = goto_checker.build_error_trace();
+        output_error_trace(error_trace, ns, trace_optionst(options), ui_message_handler);
+        report_failure(ui_message_handler);
+        goto_checker.output_error_witness(error_trace);
+        break;
+      }
+
+      case resultt::UNKNOWN:
+        report_inconclusive(ui_message_handler);
+        break;
+
+      case resultt::ERROR:
+        report_error(ui_message_handler);
+        break;
+    }
+  }
 
 protected:
   abstract_goto_modelt &goto_model;
   goto_checkerT goto_checker;
 };
-
-template<class goto_checkerT>
-void stop_on_fail_verifiert::report()
-{
-  switch(determine_result(properties))
-  {
-    case resultt::PASS:
-      report_success(ui_message_handler);
-      goto_checker.output_proof();
-      break;
-
-    case resultt::FAIL:
-    {
-      goto_tracet error_trace = goto_checker.build_error_trace();
-      output_error_trace(error_trace, ns, trace_options(), ui_message_handler);
-      report_failure(ui_message_handler);
-      goto_checker.output_error_witness(error_trace);
-      break;
-    }
-
-    case resultt::UNKNOWN:
-      report_inconclusive(ui_message_handler);
-      break;
-
-    case resultt::ERROR:
-      report_error(ui_message_handler);
-      break;
-  }
-}
 
 #endif // CPROVER_GOTO_CHECKER_STOP_ON_FAIL_VERIFIER_H
