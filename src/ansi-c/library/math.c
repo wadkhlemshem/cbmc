a807a35fe6807cce55707fbf7ab46daf22bdcc9c
Peter Schrammel <peter.schrammel@diffblue.com>
2018-11-19 17:42:02 +0000

    Add model for sin and cos

--- src/ansi-c/library/math.c
+++ src/ansi-c/library/math.c
@@ -343,129 +343,341 @@ inline int __fpclassify(double d) {
 
 /* FUNCTION: sin */
 
-double __VERIFIER_nondet_double();
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
+
+
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunSoft, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ * ====================================================
+ */
+
+#ifdef __LITTLE_ENDIAN
+#define __HI(x) *(1+(int*)&x)
+#define __LO(x) *(int*)&x
+#else
+#define __HI(x) *(int*)&x
+#define __LO(x) *(1+(int*)&x)
+#endif
+
+/* __kernel_sin( x, y, iy)
+ * kernel sin function on [-pi/4, pi/4], pi/4 ~ 0.7854
+ * Input x is assumed to be bounded by ~pi/4 in magnitude.
+ * Input y is the tail of x.
+ * Input iy indicates whether y is 0. (if iy=0, y assume to be 0). 
+ *
+ * Algorithm
+ *	1. Since sin(-x) = -sin(x), we need only to consider positive x. 
+ *	2. if x < 2^-27 (hx<0x3e400000 0), return x with inexact if x!=0.
+ *	3. sin(x) is approximated by a polynomial of degree 13 on
+ *	   [0,pi/4]
+ *		  	         3            13
+ *	   	sin(x) ~ x + S1*x + ... + S6*x
+ *	   where
+ *	
+ * 	|sin(x)         2     4     6     8     10     12  |     -58
+ * 	|----- - (1+S1*x +S2*x +S3*x +S4*x +S5*x  +S6*x   )| <= 2
+ * 	|  x 					           | 
+ * 
+ *	4. sin(x+y) = sin(x) + sin'(x')*y
+ *		    ~ sin(x) + (1-x*x/2)*y
+ *	   For better accuracy, let 
+ *		     3      2      2      2      2
+ *		r = x *(S2+x *(S3+x *(S4+x *(S5+x *S6))))
+ *	   then                   3    2
+ *		sin(x) = x + (S1*x + (x *(r-y/2)+y))
+ */
+
+double fdlibm_kernel_sin(double x, double y, int iy)
+{
+  const double 
+  half =  5.00000000000000000000e-01, /* 0x3FE00000, 0x00000000 */
+  S1  = -1.66666666666666324348e-01, /* 0xBFC55555, 0x55555549 */
+  S2  =  8.33333333332248946124e-03, /* 0x3F811111, 0x1110F8A6 */
+  S3  = -1.98412698298579493134e-04, /* 0xBF2A01A0, 0x19C161D5 */
+  S4  =  2.75573137070700676789e-06, /* 0x3EC71DE3, 0x57B1FE7D */
+  S5  = -2.50507602534068634195e-08, /* 0xBE5AE5E6, 0x8A2B9CEB */
+  S6  =  1.58969099521155010221e-10; /* 0x3DE5D93A, 0x5ACFD57C */
+	double z,r,v;
+	int ix;
+	ix = __HI(x)&0x7fffffff;	/* high word of x */
+	if(ix<0x3e400000)			/* |x| < 2**-27 */
+	   {if((int)x==0) return x;}		/* generate inexact */
+	z	=  x*x;
+	v	=  z*x;
+	r	=  S2+z*(S3+z*(S4+z*(S5+z*S6)));
+	if(iy==0) return x+v*(S1+z*r);
+	else      return x-((z*(half*y-v*r)-y)-v*S1);
+}
+
+/*
+ * __kernel_cos( x,  y )
+ * kernel cos function on [-pi/4, pi/4], pi/4 ~ 0.785398164
+ * Input x is assumed to be bounded by ~pi/4 in magnitude.
+ * Input y is the tail of x. 
+ *
+ * Algorithm
+ *	1. Since cos(-x) = cos(x), we need only to consider positive x.
+ *	2. if x < 2^-27 (hx<0x3e400000 0), return 1 with inexact if x!=0.
+ *	3. cos(x) is approximated by a polynomial of degree 14 on
+ *	   [0,pi/4]
+ *		  	                 4            14
+ *	   	cos(x) ~ 1 - x*x/2 + C1*x + ... + C6*x
+ *	   where the remez error is
+ *	
+ * 	|              2     4     6     8     10    12     14 |     -58
+ * 	|cos(x)-(1-.5*x +C1*x +C2*x +C3*x +C4*x +C5*x  +C6*x  )| <= 2
+ * 	|    					               | 
+ * 
+ * 	               4     6     8     10    12     14 
+ *	4. let r = C1*x +C2*x +C3*x +C4*x +C5*x  +C6*x  , then
+ *	       cos(x) = 1 - x*x/2 + r
+ *	   since cos(x+y) ~ cos(x) - sin(x)*y 
+ *			  ~ cos(x) - x*y,
+ *	   a correction term is necessary in cos(x) and hence
+ *		cos(x+y) = 1 - (x*x/2 - (r - x*y))
+ *	   For better accuracy when x > 0.3, let qx = |x|/4 with
+ *	   the last 32 bits mask off, and if x > 0.78125, let qx = 0.28125.
+ *	   Then
+ *		cos(x+y) = (1-qx) - ((x*x/2-qx) - (r-x*y)).
+ *	   Note that 1-qx and (x*x/2-qx) is EXACT here, and the
+ *	   magnitude of the latter is at least a quarter of x*x/2,
+ *	   thus, reducing the rounding error in the subtraction.
+ */
+
+double fdlibm_kernel_cos(double x, double y)
+{
+  const double 
+  one =  1.00000000000000000000e+00, /* 0x3FF00000, 0x00000000 */
+  C1  =  4.16666666666666019037e-02, /* 0x3FA55555, 0x5555554C */
+  C2  = -1.38888888888741095749e-03, /* 0xBF56C16C, 0x16C15177 */
+  C3  =  2.48015872894767294178e-05, /* 0x3EFA01A0, 0x19CB1590 */
+  C4  = -2.75573143513906633035e-07, /* 0xBE927E4F, 0x809C52AD */
+  C5  =  2.08757232129817482790e-09, /* 0x3E21EE9E, 0xBDB4B1C4 */
+  C6  = -1.13596475577881948265e-11; /* 0xBDA8FAE9, 0xBE8838D4 */
+	double a,hz,z,r,qx;
+	int ix;
+	ix = __HI(x)&0x7fffffff;	/* ix = |x|'s high word*/
+	if(ix<0x3e400000) {			/* if x < 2**27 */
+	    if(((int)x)==0) return one;		/* generate inexact */
+	}
+	z  = x*x;
+	r  = z*(C1+z*(C2+z*(C3+z*(C4+z*(C5+z*C6)))));
+	if(ix < 0x3FD33333) 			/* if |x| < 0.3 */ 
+	    return one - (0.5*z - (z*r - x*y));
+	else {
+	    if(ix > 0x3fe90000) {		/* x > 0.78125 */
+		qx = 0.28125;
+	    } else {
+	        __HI(qx) = ix-0x00200000;	/* x/4 */
+	        __LO(qx) = 0;
+	    }
+	    hz = 0.5*z-qx;
+	    a  = one-qx;
+	    return a - (hz - (z*r-x*y));
+	}
+}
+
+/* __ieee754_rem_pio2(x,y)
+ * 
+ * return the remainder of x rem pi/2 in y[0]+y[1]
+ * use __kernel_rem_pio2()
+ */
+// an approximation of __ieee754_rem_pio2
+int fdlibm_ieee754_rem_pio2_approx(double x, double *y)
+{
+  return fmod(x, M_PI_2);
+}
+
+/* sin(x)
+ * Return sine function of x.
+ *
+ * kernel function:
+ *	__kernel_sin		... sine function on [-pi/4,pi/4]
+ *	__kernel_cos		... cose function on [-pi/4,pi/4]
+ *	__ieee754_rem_pio2	... argument reduction routine
+ *
+ * Method.
+ *      Let S,C and T denote the sin, cos and tan respectively on 
+ *	[-PI/4, +PI/4]. Reduce the argument x to y1+y2 = x-k*pi/2 
+ *	in [-pi/4 , +pi/4], and let n = k mod 4.
+ *	We have
+ *
+ *          n        sin(x)      cos(x)        tan(x)
+ *     ----------------------------------------------------------
+ *	    0	       S	   C		 T
+ *	    1	       C	  -S		-1/T
+ *	    2	      -S	  -C		 T
+ *	    3	      -C	   S		-1/T
+ *     ----------------------------------------------------------
+ *
+ * Special cases:
+ *      Let trig be any of sin, cos, or tan.
+ *      trig(+-INF)  is NaN, with signals;
+ *      trig(NaN)    is that NaN;
+ *
+ * Accuracy:
+ *	TRIG(x) returns trig(x) nearly rounded 
+ */
+
+double fdlibm_sin(double x)
+{
+	double y[2],z=0.0;
+	int n, ix;
+
+    /* High word of x. */
+	ix = __HI(x);
+
+    /* |x| ~< pi/4 */
+	ix &= 0x7fffffff;
+	if(ix <= 0x3fe921fb) return fdlibm_kernel_sin(x,z,0);
+
+    /* sin(Inf or NaN) is NaN */
+	else if (ix>=0x7ff00000) return x-x;
+
+    /* argument reduction needed */
+	else {
+    x = fmod(x + M_PI, M_PI * 2) - M_PI; // restrict x so that -M_PI < x < M_PI
+n = fdlibm_ieee754_rem_pio2_approx(x,y);
+	    switch(n&3) {
+		case 0: return  fdlibm_kernel_sin(y[0],y[1],1);
+		case 1: return  fdlibm_kernel_cos(y[0],y[1]);
+		case 2: return -fdlibm_kernel_sin(y[0],y[1],1);
+		default:
+			return -fdlibm_kernel_cos(y[0],y[1]);
+	    }
+	}
+}
 
 double sin(double x)
 {
-  // gross over-approximation
-  double ret=__VERIFIER_nondet_double();
-
-  if(__CPROVER_isinfd(x) || __CPROVER_isnand(x))
-    __CPROVER_assume(__CPROVER_isnand(ret));
-  else
-  {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==0);
-  }
-
-  return ret;
+  return fdlibm_sin(x);
+  //return cos(x - M_PI_2);
 }
 
 /* FUNCTION: sinl */
+
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
 
 long double __VERIFIER_nondet_long_double();
 
 long double sinl(long double x)
 {
-  // gross over-approximation
-  long double ret=__VERIFIER_nondet_long_double();
-
-  if(__CPROVER_isinfld(x) || __CPROVER_isnanld(x))
-    __CPROVER_assume(__CPROVER_isnanld(ret));
-  else
-  {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==0);
-  }
-
-  return ret;
+  return cosl(x - M_PI_2);
 }
 
 /* FUNCTION: sinf */
+
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
 
 float __VERIFIER_nondet_float();
 
 float sinf(float x)
 {
-  // gross over-approximation
-  float ret=__VERIFIER_nondet_float();
-
-  if(__CPROVER_isinff(x) || __CPROVER_isnanf(x))
-    __CPROVER_assume(__CPROVER_isnanf(ret));
-  else
-  {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==0);
-  }
-
-  return ret;
+  return cosf(x - M_PI_2);
 }
 
 /* FUNCTION: cos */
+
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
 
 double __VERIFIER_nondet_double();
 
 double cos(double x)
 {
-  // gross over-approximation
-  double ret=__VERIFIER_nondet_double();
-
-  if(__CPROVER_isinfd(x) || __CPROVER_isnand(x))
-    __CPROVER_assume(__CPROVER_isnand(ret));
-  else
+  return sin(x + M_PI_2);
+/*
+  double t, s;
+  int p;
+  p = 0;
+  s = 1.0;
+  t = 1.0;
+  x = fmod(x + M_PI, M_PI * 2) - M_PI; // restrict x so that -M_PI < x < M_PI
+  double xsqr = x * x;
+  double ab = 1;
+  while((ab > 1e-16) && (p < 15))
   {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==1);
+    p++;
+    t = (-t * xsqr) / (((p << 1) - 1) * (p << 1));
+    s += t;
+    ab = (s == 0) ? 1 : fabs(t / s);
   }
-
-  return ret;
+  return s;*/
 }
 
 /* FUNCTION: cosl */
+
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
 
 long double __VERIFIER_nondet_long_double();
 
 long double cosl(long double x)
 {
-  // gross over-approximation
-  long double ret=__VERIFIER_nondet_long_double();
-
-  if(__CPROVER_isinfld(x) || __CPROVER_isnanld(x))
-    __CPROVER_assume(__CPROVER_isnanld(ret));
-  else
+  long double t, s;
+  int p;
+  p = 0;
+  s = 1.0;
+  t = 1.0;
+  x = fmod(x + M_PI, M_PI * 2) - M_PI; // restrict x so that -M_PI < x < M_PI
+  long double xsqr = x * x;
+  long double ab = 1;
+  while((ab > 1e-16) && (p < 15))
   {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==1);
+    p++;
+    t = (-t * xsqr) / (((p << 1) - 1) * (p << 1));
+    s += t;
+    ab = (s == 0) ? 1 : fabs(t / s);
   }
-
-  return ret;
+  return s;
 }
 
 /* FUNCTION: cosf */
+
+#ifndef __CPROVER_MATH_H_INCLUDED
+#include <math.h>
+#define __CPROVER_MATH_H_INCLUDED
+#endif
 
 float __VERIFIER_nondet_float();
 
 float cosf(float x)
 {
-__CPROVER_hide:;
-  // gross over-approximation
-  float ret=__VERIFIER_nondet_float();
-
-  if(__CPROVER_isinff(x) || __CPROVER_isnanf(x))
-    __CPROVER_assume(__CPROVER_isnanf(ret));
-  else
+  float t, s;
+  int p;
+  p = 0;
+  s = 1.0;
+  t = 1.0;
+  x = fmod(x + M_PI, M_PI * 2) - M_PI; // restrict x so that -M_PI < x < M_PI
+  float xsqr = x * x;
+  float ab = 1;
+  while((ab > 1e-16) && (p < 15))
   {
-    __CPROVER_assume(ret<=1);
-    __CPROVER_assume(ret>=-1);
-    __CPROVER_assume(x!=0 || ret==1);
+    p++;
+    t = (-t * xsqr) / (((p << 1) - 1) * (p << 1));
+    s += t;
+    ab = (s == 0) ? 1 : fabs(t / s);
   }
-
-  return ret;
+  return s;
 }
 
 /* FUNCTION: __builtin_nan */
