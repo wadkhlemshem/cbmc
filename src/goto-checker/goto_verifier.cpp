bb516316b62dca033ea56818a4ffde05116a51e3
Peter Schrammel <peter.schrammel@diffblue.com>
2018-12-16 11:32:43 +0000

    goto_verifier add coloured reporting

--- src/goto-checker/goto_verifier.cpp
+++ src/goto-checker/goto_verifier.cpp
@@ -11,7 +11,6 @@ Author: Daniel Kroening, Peter Schrammel
 
 #include "goto_verifier.h"
 
-#include <util/json_stream.h>
 #include <util/xml.h>
 
 #include "bmc_util.h"
@@ -32,23 +31,101 @@ void goto_verifiert::report()
   {
     case ui_message_handlert::uit::PLAIN:
     {
-      result() << "\n** Results:" << eom;
+      log.result() << "\n** Results:" << messaget::eom;
 
-      for(const auto &property_pair : properties)
+      // collect properties in a vector
+      std::vector<propertiest::const_iterator> sorted_properties;
+
+      for(auto p_it = properties.begin(); p_it != properties.end(); p_it++)
+        sorted_properties.push_back(p_it);
+
+      // now determine an ordering for those goals:
+      // 1. alphabetical ordering of file name
+      // 2. numerical ordering of line number
+      // 3. alphabetical ordering of goal ID
+      std::sort(
+        sorted_properties.begin(),
+        sorted_properties.end(),
+        [](propertiest::const_iterator pit1, propertiest::const_iterator pit2) {
+          const auto &p1 = pit1->second.location->source_location;
+          const auto &p2 = pit2->second.location->source_location;
+          if(p1.get_file() != p2.get_file())
+            return id2string(p1.get_file()) < id2string(p2.get_file());
+          else if(!p1.get_line().empty() && !p2.get_line().empty())
+            return std::stoul(id2string(p1.get_line())) <
+                   std::stoul(id2string(p2.get_line()));
+          else
+            return id2string(pit1->first) < id2string(pit2->first);
+        });
+
+      // now show in the order we have determined
+
+      irep_idt previous_function;
+      irep_idt current_file;
+      for(const auto &p : sorted_properties)
       {
-        result() << as_string(property_pair.first, property_pair.second)
-                 << eom;
+        const auto &l = p->second.location->source_location;
+
+        if(l.get_function() != previous_function)
+        {
+          if(!previous_function.empty())
+            log.result() << '\n';
+          previous_function = l.get_function();
+          if(!previous_function.empty())
+          {
+            current_file = l.get_file();
+            if(!current_file.empty())
+              log.result() << current_file << ' ';
+            if(!l.get_function().empty())
+              log.result() << "function " << l.get_function();
+            log.result() << messaget::eom;
+          }
+        }
+
+        log.result() << messaget::faint << '[' << p->first << "] " << messaget::reset;
+
+        if(l.get_file() != current_file)
+          log.result() << "file " << l.get_file() << ' ';
+
+        if(!l.get_line().empty())
+          log.result() << "line " << l.get_line() << ' ';
+
+        log.result() << p->second.description << ": ";
+
+        switch(p->second.result)
+        {
+          case property_resultt::NOT_REACHED:
+            log.result() << messaget::magenta;
+            break;
+          case property_resultt::UNKNOWN:
+            log.result() << messaget::bright_magenta;
+            break;
+          case property_resultt::NOT_REACHABLE:
+            log.result() << messaget::bright_green;
+            break;
+          case property_resultt::PASS:
+            log.result() << messaget::green;
+            break;
+          case property_resultt::FAIL:
+            log.result() << messaget::red;
+            break;
+          case property_resultt::ERROR:
+            log.result() << messaget::bright_red;
+            break;
+        }
+
+        log.result() << as_string(p->second.result) << messaget::reset << messaget::eom;
       }
 
-      status() << "\n** " << count_properties(properties, resultt::FAIL)
-               << " of " << properties.size() << " failed" << eom;
+      log.status() << "\n** " << count_properties(properties, property_resultt::FAIL)
+               << " of " << properties.size() << " failed" << messaget::eom;
       break;
     }
     case ui_message_handlert::uit::XML_UI:
     {
       for(const auto &property_pair : properties)
       {
-        result() << xml(property_pair.first, property_pair.second);
+        log.result() << xml(property_pair.first, property_pair.second);
       }
       break;
     }
