5ddd446626d60a766fd8de1e571ac0949c938055
Peter Schrammel <peter.schrammel@diffblue.com>
2018-12-16 12:44:14 +0000

    Add bmc checker

---
+++ src/goto-checker/bmc_checker.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************\
+
+Module: Goto Checker using Bounded Model Checking
+
+Author: Daniel Kroening, Peter Schrammel
+
+\*******************************************************************/
+
+/// \file
+/// Goto Checker using Bounded Model Checking
+
+#include "bmc_checker.h"
+
+bmc_checkert::bmc_checkert(
+  const optionst &options,
+  ui_message_handlert &ui_message_handler,
+  abstract_goto_modelt &goto_model)
+  : goto_checkert(options, ui_message_handler),
+    goto_model(goto_model)
+{
+}
+
+propertiest bmc_checkert::operator()(const propertiest &properties)
+{
+  return properties;
+}
+
+goto_tracet bmc_checkert::build_error_trace() const
+{
+  goto_tracet goto_trace;
+  return goto_trace;
+}
