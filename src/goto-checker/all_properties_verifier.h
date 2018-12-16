330a1633059c0a42ecd7c782a8535651b4cb13c7
Peter Schrammel <peter.schrammel@diffblue.com>
2018-12-16 12:44:31 +0000

    Add all properties verifier

---
+++ src/goto-checker/all_properties_verifier.h
@@ -0,0 +1,41 @@
+/*******************************************************************\
+
+Module: Goto Verifier for Verifying all Properties
+
+Author: Daniel Kroening, Peter Schrammel
+
+\*******************************************************************/
+
+/// \file
+/// Goto Verifier for Verifying all Properties
+
+#ifndef CPROVER_GOTO_CHECKER_ALL_PROPERTIES_VERIFIER_H
+#define CPROVER_GOTO_CHECKER_ALL_PROPERTIES_VERIFIER_H
+
+#include "goto_verifier.h"
+
+template<class goto_checkerT>
+class all_properties_verifiert : public goto_verifiert
+{
+public:
+  all_properties_verifiert(
+    const optionst &,
+    ui_message_handlert &,
+    abstract_goto_modelt &)
+  : goto_verifiert(options, ui_message_handler),
+    goto_model(goto_model),
+    goto_checker(options, ui_message_handler, goto_model)
+  {
+  }
+
+  resultt operator()() override
+  {
+    return resultt::UNKNOWN;
+  }
+
+protected:
+  abstract_goto_modelt &goto_model;
+  goto_checkerT goto_checker;
+};
+
+#endif // CPROVER_GOTO_CHECKER_ALL_PROPERTIES_VERIFIER_H
