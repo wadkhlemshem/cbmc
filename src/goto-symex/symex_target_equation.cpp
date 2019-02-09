cc977321bc6e6e4583179324b864b5b985e9182b
Peter Schrammel <peter.schrammel@diffblue.com>
2019-02-09 23:16:15 +0000

    Hidden field is used for hiding in trace

--- src/goto-symex/symex_target_equation.cpp
+++ src/goto-symex/symex_target_equation.cpp
@@ -533,7 +533,7 @@ void symex_target_equationt::convert_assertions(
     {
       // ignore already converted assertions in the error trace
       if(step.is_assert() && step.converted)
-        step.ignore = true;
+        step.hidden = true;
 
       if(step.is_assert() && !step.ignore && !step.converted)
       {
@@ -560,7 +560,7 @@ void symex_target_equationt::convert_assertions(
   {
     // ignore already converted assertions in the error trace
     if(step.is_assert() && step.converted)
-      step.ignore = true;
+      step.hidden = true;
 
     if(step.is_assert() && !step.ignore && !step.converted)
     {
